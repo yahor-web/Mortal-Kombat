cc1c35dc04224a08cc1b11de1915b912286c95d2
yahor-web <71457422+yahor-web@users.noreply.github.com>
2021-03-19 20:15:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$ (Document).ready(function () {
+$('#app').vide({
+  mp4: "video/video.mp4",
+  webm: "video/video.webm",
+  ogv: "video/video.ogv",
+  poster: "img/cover-image.jpg"
+});
+});
+
+
+const modal = document.querySelector('.modal');
+const primaryBtn = document.querySelector('.button-primary');
+
+const toggleHandler = () => {
+    modal.classList.toggle('modalHide')
+}
+
+primaryBtn.addEventListener('click', toggleHandler);
+modal.addEventListener('click', toggleHandler);

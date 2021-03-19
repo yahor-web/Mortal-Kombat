7cf259a569872b471ddbda7f261bf9482cd9d818
yahor-web <71457422+yahor-web@users.noreply.github.com>
2021-03-19 21:15:47 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $ (Document).ready(function () {
 $('#app').vide({
-  mp4: 'video/video.mp4',
-  webm: 'video/video.webm',
-  ogv: 'video/video.ogv',
-  poster: 'img/cover-image.jpg'
+  mp4: "video/video.mp4",
+  webm: "video/video.webm",
+  ogv: "video/video.ogv",
+  poster: "img/cover-image.jpg"
 });
 });
 
